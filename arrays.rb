08a93670d1f669f63814a346036cb136ed9171ed
Laura Moore <rileyle@gmail.com>
2017-01-31 12:29:33 -0500

    First push

---
+++ arrays.rb
@@ -0,0 +1,6 @@
+our_class = ["Kalea", "Nancy", "David", "Ron", "Laura", "Dave", "Demtra", "Kendrick", "Phil", "Ben", "Michael", "Miguel"]
+
+
+sentence = "Ruby is actually kind of fun once you get used to it."
+words = sentence.split
+words.select {|word| word.length == 4 }
